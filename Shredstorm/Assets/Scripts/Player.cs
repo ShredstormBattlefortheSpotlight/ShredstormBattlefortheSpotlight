6ee2c702d3fed5a11b010407bced0dde489de82f
pauluhlenkott <68128585+pauluhlenkott@users.noreply.github.com>
2025-06-10 11:25:24 -0700

    Added Movement for Character

--- Shredstorm/Assets/Scripts/Player.cs
+++ Shredstorm/Assets/Scripts/Player.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private int health;
+    private float health;
     [SerializeField]
-    private int damage;
+    private float damage;
     [SerializeField]
     private int experience;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Current forward: " + transform.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement = Vector3.ClampMagnitude(movement, 1f);
+        if (movement != Vector3.zero)
+        {
+            transform.forward = movement;
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation(transform.forward), Time.deltaTime * 40f);
+            transform.rotation = Quaternion.Euler(-90, transform.eulerAngles.y, transform.eulerAngles.z);
 
+        }
+        controller.Move(speed * Time.deltaTime * movement);
     }
 }
